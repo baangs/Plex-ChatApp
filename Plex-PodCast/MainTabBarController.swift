a508094bb2145e0a147db31eb05ea4dd448ba3b4
baangs <aey.baangs@gmail.com>
2020-09-07 12:38:08 -0700

    first commit, setup green

---
+++ Plex-PodCast/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  Plex-PodCast
+//
+//  Created by RD on 9/7/20.
+//  Copyright © 2020 Reggie Daniels. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarContoller: UITabBarController {
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+        view.backgroundColor = .green
+        
+
+    }
+}
---
+++ Plex-PodCast/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Plex-PodCast
+//
+//  Created by RD on 9/4/20.
+//  Copyright © 2020 Reggie Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+        
+     
+    
+
+    }
+}
+
